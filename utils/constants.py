985ac3d18c1af38c8c2e7232f2fb21536ebe0f15
walter-souza <41118985+walter-souza@users.noreply.github.com>
2023-06-04 13:48:57 -0300

    Delete constants.py

--- utils/constants.py
+++
@@ -1,9 +0,0 @@
-''' =============== Constants ==============='''
-DATASET_REDUCTION = 0.1
-SEED = 21
-BATCH_SIZE = 128
-NUM_CLASSES = 62
-EMBEDDING_SIZE = 256   #TODO: Alterar no modelo para ser dinâmico
-TOKEN_MAX_LENGTH = 128 #TODO: Alterar no modelo para ser dinâmico
-EPOCHS = 20
-BASE_MODEL = "distilbert-base-uncased"
