3139af0a02d3b35cdcffbb4979cd23727eac6b6d
walter-souza <41118985+walter-souza@users.noreply.github.com>
2023-06-04 13:48:07 -0300

    Refactoring + Tests

--- utils/dml_classification_report.py
+++ utils/dml_classification_report.py
@@ -1,63 +1,36 @@
 import numpy as np
-import torch
-import transformers
 import matplotlib.pyplot as plt
-from tqdm import tqdm
-import pandas as pd
-from sklearn.manifold import TSNE
 import seaborn as sns
-from pytorch_metric_learning import miners, losses
 from sklearn.model_selection import train_test_split
-from sklearn.model_selection import StratifiedKFold
-from sklearn.model_selection import train_test_split
-from sklearn.model_selection import cross_val_score
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.metrics import confusion_matrix
-from utils.data_visualization import *
-from utils.constants import *
-import sklearn.neighbors
 import sklearn.metrics as metrics
+from utils import dataset_utils
+import os
 
-def data_classification_report(dl, model, step, device, neighbors):
-    xall = None
-    yall = []
-    with torch.no_grad():
-        for ii,item in enumerate(dl):
-            input_ids = item['input_ids'].to(device)
-            attention_mask = item['attention_mask'].to(device)
-            labels = item['labels'].to(device)
-            labels = labels.long()
-
-            embeddings = model(input_ids, attention_mask)
-            if xall is None:
-                xall = embeddings.data.cpu().numpy()
-            else:
-                xall = np.concatenate((xall, embeddings.data.cpu().numpy()))
-            yall = np.concatenate((yall, labels.data.cpu().numpy()))
+def get_metrics(true, predict, step):
+    acc = metrics.accuracy_score(true, predict)
+    precision = metrics.precision_score(true, predict, average='macro')
+    recall = metrics.recall_score(true, predict, average='macro')
+    f1 = metrics.f1_score(true, predict, average='macro')
     
-    xtrain, xtest, ytrain, ytest = train_test_split(xall, yall, test_size=0.33, random_state=123,stratify=np.array(yall))
-        
-    knn = sklearn.neighbors.KNeighborsClassifier(n_neighbors=neighbors,weights='distance')
-    knn.fit(xtrain,ytrain)
-
-    ypred = knn.predict(xtest)
-    ypredp = knn.predict_proba(xtest)
-
-    acc = metrics.accuracy_score(ytest, ypred)
-    precision = metrics.precision_score(ytest,ypred,average='macro')
-    recall = metrics.recall_score(ytest,ypred,average='macro')
-    f1 = metrics.f1_score(ytest,ypred,average='macro')
-    top_k_2 = metrics.top_k_accuracy_score(ytest,ypredp,k=2)
-    top_k_3 = metrics.top_k_accuracy_score(ytest,ypredp,k=3)
-    top_k_4 = metrics.top_k_accuracy_score(ytest,ypredp,k=4)
-        
     mtr = {"accuracy_"+step: acc, 
             "precision_"+step: precision, 
             "recall_"+step: recall, 
-            "f1_"+step: f1, 
-            "top_k_2_"+step: top_k_2, 
-            "top_k_3_"+step: top_k_3, 
-            "top_k_4_"+step: top_k_4}
+            "f1_"+step: f1}
+    return mtr
+
+def data_classification_report(dl, model, step, device, neighbors):
+    xall, yall = dataset_utils.get_dml_model_outputs(dl, model, device)
+    
+    xtrain, xtest, ytrain, ytest = train_test_split(xall, yall, test_size=0.33, random_state=123, stratify=np.array(yall))
+        
+    knn = KNeighborsClassifier(n_neighbors=neighbors, weights='distance')
+    knn.fit(xtrain,ytrain)
+
+    ypred = knn.predict(xtest)
+    
+    mtr = get_metrics(ytest, ypred, step)
     report = metrics.classification_report(ytest, ypred, output_dict=True, zero_division=0)
     cm = confusion_matrix(ytest, ypred)
     return mtr, report, cm
@@ -65,9 +38,11 @@ def data_classification_report(dl, model, step, device, neighbors):
 
 def dml_data_classification_report(model, data_loader, device, neighbors, epoch, save_path, step):
     model.eval()
-    neighbors = 3
     mtr, report, cm = data_classification_report(data_loader, model, step, device, neighbors=neighbors)
     
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
     if epoch == 0:
         result_file = open('{}/results.csv'.format(save_path), 'w+')
         result_file.write('epoch,')
@@ -81,8 +56,41 @@ def dml_data_classification_report(model, data_loader, device, neighbors, epoch,
     if not os.path.exists(cms_path):
         os.makedirs(cms_path)
 
+    np.savetxt('{}/dml_cm_test_epoch{}_n{}.csv'.format(cms_path, epoch, neighbors), cm, delimiter=',', fmt='%i')
+
     plt.figure(figsize=(30,20))
     cm_plot = sns.heatmap(cm, annot=True)
     fig = cm_plot.get_figure()
     fig.savefig('{}/dml_cm_test_epoch{}_n{}.png'.format(cms_path, epoch, neighbors))
+    
     model.train()
+
+if (__name__ == '__main__'):
+    import sys, transformers, torch
+    parent = os.path.abspath('.')
+    sys.path.insert(1, parent)
+    from dml_distilbert import DMLDistilBert
+    import datasets, argparse, parameters
+
+    parser = argparse.ArgumentParser()
+    parser = parameters.training_parameters(parser)
+    config = parser.parse_args()
+
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+    print('Device:', device)
+
+    print('Importing tokenizer...')
+    tokenizer = transformers.DistilBertTokenizerFast.from_pretrained(config.base_model)
+
+    print('Importing model...')
+    model = DMLDistilBert(config.embedding_size, config.token_size, config=config.base_model)
+    model.to(device)
+    config.tokenizer = tokenizer
+
+    print('Importing dataset...')
+    dl_train, dl_test, n_classes = datasets.select('imdb62', config)
+
+    print('Classification report...')
+    dml_data_classification_report(model, dl_test, device, 3, 0, '_results/unit_tests', 'test')
+
+    print('Done!')
